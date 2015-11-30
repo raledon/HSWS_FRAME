656eb16fd71f7b483706910f977824fb94b41418
john <john@home-pc>
2015-11-30 12:14:57 +0800

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,11 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'LOAD_EXT_FILE' => 'function',
+    'TMPL_PARSE_STRING' => array(
+        '__PUBLIC__' => '',
+        '__CSS__' =>'',
+        '__JS__' => '',
+        '__IMG__' => '',
+    ),
 );
